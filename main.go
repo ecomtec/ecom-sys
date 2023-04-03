4adb561fbd239b442e17aa0f5fce84fa59b69dd4
suhailfinmont <109333202+suhailfinmont@users.noreply.github.com>
2023-04-03 16:12:36 +0530

    Gin Routers
    API calls

--- main.go
+++ main.go
@@ -1,10 +1,47 @@
 package main
 
 import (
-	"log"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Create a new Gin router
+	router := gin.Default()
 
-	log.Println(" Initial commit")
+	// Define your API routes
+	router.GET("/hello", helloHandler)
+	router.POST("/users", createUserHandler)
+	router.GET("/users/:id", getUserHandler)
+	router.PUT("/users/:id", updateUserHandler)
+	router.DELETE("/users/:id", deleteUserHandler)
+
+	// Start the server
+	router.Run(":8080")
+}
+
+func helloHandler(c *gin.Context) {
+	// Return a simple message
+	c.JSON(200, gin.H{
+		"message": "Hello, Suhail.....!",
+	})
+}
+
+func createUserHandler(c *gin.Context) {
+	// Create a new user
+	// ...
+}
+
+func getUserHandler(c *gin.Context) {
+	// Get a user by ID
+	// ...
+}
+
+func updateUserHandler(c *gin.Context) {
+	// Update a user by ID
+	// ...
+}
+
+func deleteUserHandler(c *gin.Context) {
+	// Delete a user by ID
+	// ...
 }
